0fc29f76e5c6c866534a35fc1f86cdffc58d1d2b
Jessica Tagudar <tagudarj@mail.gvsu.edu>
2020-12-03 19:15:56 +0000

    12/2 Class

--- Project8/app.R
+++ Project8/app.R
@@ -6,46 +6,39 @@ library(ggplot2)
 # Load data ----
 cereal <- read_csv("data/cereal.csv")
 
-
-# User interface ----
-ui <- (fluidPage(    
-    
-    # Give the page a title
-    titlePanel("Cereal Information"),
-    
-    # Generate a row with a sidebar
-    sidebarLayout(      
-        
-        # Define the sidebar with one input
-        sidebarPanel(
-            selectInput("column", "Column:", 
-                        choices=colnames(cereal)),
-            hr(),
-            helpText("Data from US Cereal Dataset.")
-        ),
-        
-        # Create a spot for the barplot
-        mainPanel(
-            plotOutput("cerealPlot")  
-        )
-        
-    )
-)   
+ui <- basicPage(
+    plotOutput("plot1",
+               click = "plot_click",
+               dblclick = "plot_dblclick",
+               hover = "plot_hover",
+               brush = "plot_brush"
+    ),
+    verbatimTextOutput("info")
 )
 
-# Server logic ----
 server <- function(input, output) {
+    output$plot1 <- renderPlot({
+        plot(cereal$shelf, cereal$rating)
+    })
     
-    # Fill in the spot we created for a plot
-    output$cerealPlot <- renderPlot({
+    output$info <- renderText({
+        xy_str <- function(e) {
+            if(is.null(e)) return("NULL\n")
+            paste0("x=", round(e$x, 1), " y=", round(e$y, 1), "\n")
+        }
+        xy_range_str <- function(e) {
+            if(is.null(e)) return("NULL\n")
+            paste0("xmin=", round(e$xmin, 1), " xmax=", round(e$xmax, 1), 
+                   " ymin=", round(e$ymin, 1), " ymax=", round(e$ymax, 1))
+        }
         
-        # Render a barplot
-        barplot(cereal[,input$column]*1000, 
-                main=input$column,
-                ylab="Freqency",
-                xlab="Variable")
+        paste0(
+            "click: ", xy_str(input$plot_click),
+            "dblclick: ", xy_str(input$plot_dblclick),
+            "hover: ", xy_str(input$plot_hover),
+            "brush: ", xy_range_str(input$plot_brush)
+        )
     })
 }
 
-# Run app ----
 shinyApp(ui, server)
